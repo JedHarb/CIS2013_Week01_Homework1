c8e7383fe9d0e8303d2f36532184af0b8c70bd69
Jared H <35547112+JedHarb@users.noreply.github.com>
2018-01-23 19:42:48 -0600

    Week 1 Business Card program
    Set up basically the same as my project 2 makeup work, but I also found that a tab in C++ is equal to 8 spaces, and was able to mix these to space the contact information correctly.

---
+++ BusinessCard.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "===========================================================\n";
+	cout << "\n";
+	cout << "Jared Harber\n";
+	cout << "\n";
+	cout << "Technical Support, Farmers Insurance - Oklahoma City\n";
+	cout << "\n";
+	cout << "2424 NW 157 Ter 		p: (405) 706-1963\n";
+	cout << "Edmond, OK      		e: jedharb@gmail.com\n";
+	cout << "				g: github.com/JedHarb\n";
+	cout << "				t: None\n";
+	cout << "\n";
+	cout << "===========================================================";
+	
+	return 0;
+}
